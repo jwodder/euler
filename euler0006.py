35988f3291afa44e602d4c39b6daa5d8096ef3d4
John T. Wodder II <jwodder@sdf.lonestar.org>
2014-07-03 23:16:17 -0400

    Added the problem descriptions as docstrings in every solution so far

--- euler0006.py
+++ euler0006.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python
+r"""The sum of the squares of the first ten natural numbers is,
+
+    $$1^2 + 2^2 + \cdots + 10^2 = 385$$
+
+    The square of the sum of the first ten natural numbers is,
+
+    $$(1 + 2 + ... + 10)^2 = 55^2 = 3025$$
+
+    Hence the difference between the sum of the squares of the first ten
+    natural numbers and the square of the sum is $3025 - 385 = 2640$.
+
+    Find the difference between the sum of the squares of the first one hundred
+    natural numbers and the square of the sum."""
+
 n = 100
 print (n*(n+1)//2)**2 - (n * (n+1) * (2*n + 1) // 6)
