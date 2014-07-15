eda246c8a8020b624abc8d83afdeeca6fc9dfcfd
John T. Wodder II <jwodder@sdf.lonestar.org>
2014-07-15 16:54:58 -0400

    Tried to solve #146

---
+++ dev/euler0146.hs
@@ -0,0 +1,27 @@
+-- |Investigating a Prime Pattern
+--
+-- The smallest positive integer $n$ for which the numbers $n^2+1$, $n^2+3$,
+-- $n^2+7$, $n^2+9$, $n^2+13$, and $n^2+27$ are consecutive primes is 10.  The
+-- sum of all such integers $n$ below one-million is 1242490.
+--
+-- What is the sum of all such integers $n$ below 150 million?
+
+import Primes (primes)
+
+offsets :: [Integer]
+offsets = [1,3,7,9,13,27]
+
+supN :: Integer
+-- supN = 150000000
+supN = 1000000
+
+main :: IO ()
+main = print $ sum $ acceptable [10, 12 .. supN-1] primes
+
+acceptable :: [Integer] -> [Integer] -> [Integer]
+acceptable [] _ = []
+acceptable (n:xs) piter = if map (+sq) offsets == take 6 piter'
+			  then n : acceptable xs piter'
+			  else     acceptable xs piter'
+ where sq = n*n
+       piter' = dropWhile (<= sq) piter
