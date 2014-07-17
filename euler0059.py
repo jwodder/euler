b6c72e9302a85d9947e952585adb6fa226d55f7b
John T. Wodder II <jwodder@sdf.lonestar.org>
2014-07-17 14:24:11 -0400

    Greatly sped up the solution to #59

--- euler0059.py
+++ euler0059.py
@@ -31,7 +31,6 @@
 from __future__ import with_statement
 from itertools  import cycle
 from string     import ascii_letters, digits
-from eulerlib   import cross
 
 with open('data/cipher1.txt') as fp:
     bytes = map(int, fp.read().split(','))
@@ -42,15 +41,32 @@ acceptable = map(ord, ascii_letters + digits + " .,();!'")
 # to find the correct decrypted text, and identifying the non-letters used.
 
 lowercase = range(ord('a'), ord('z')+1)
-for abc in cross(lowercase, lowercase, lowercase):
+passchars = (lowercase[:], lowercase[:], lowercase[:])
+indices = [0, 0, 0]
+while True:
+    abc = tuple(arg[i] for (arg, i) in zip(passchars, indices))
     accum = 0
-    for (byte, passc) in zip(bytes, cycle(abc)):
+    for (i, (byte, passc)) in enumerate(zip(bytes, cycle(abc))):
 	byte ^= passc
 	#if 0x20 <= byte <= 0x7E: accum += byte
 	if byte in acceptable: accum += byte
-	else: break
+	else:
+	    i %= 3
+	    passchars[i].remove(passc)
+	    for j in range(i+1, 3):
+		indices[j] = len(passchars[j])
+	    indices[i] -= 1
+	    break
     else:
 	#print ''.join(map(chr, abc))
 	#print ''.join(chr(b^p) for (b,p) in zip(bytes, cycle(abc)))
 	print accum
+	break
+    for i in range(len(indices)-1, -1, -1):
+	indices[i] += 1
+	if indices[i] >= len(passchars[i]):
+	    indices[i] = 0
+	else:
+	    break
+    else:
 	break
