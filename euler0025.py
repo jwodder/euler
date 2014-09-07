ed0f6399c1fb9061999d2454e14f9479a978e826
John T. Wodder II <jwodder@sdf.lonestar.org>
2014-09-07 18:50:24 -0400

    Solved #25

---
+++ euler0025.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+r"""1000-digit Fibonacci number
+
+    The Fibonacci sequence is defined by the recurrence relation:
+
+    $$F_n = F_{n-1} + F_{n-2}\mbox{, where } F_1 = 1 \mbox{ and } F_2 = 1.$$
+
+    Hence the first 12 terms will be:
+
+    \begin{eqnarray*}
+    F_1 & = & 1 \\
+    F_2 & = & 1 \\
+    F_3 & = & 2 \\
+    F_4 & = & 3 \\
+    F_5 & = & 5 \\
+    F_6 & = & 8 \\
+    F_7 & = & 13 \\
+    F_8 & = & 21 \\
+    F_9 & = & 34 \\
+    F_{10} & = & 55 \\
+    F_{11} & = & 89 \\
+    F_{12} & = & 144
+    \end{eqnarray*}
+
+    The 12th term, $F_{12}$, is the first term to contain three digits.
+
+    What is the first term in the Fibonacci sequence to contain 1000 digits?"""
+
+from math import ceil, log10, sqrt
+
+# ceil(log(10**999 * sqrt(5), phi))
+print int(ceil((999 + log10(sqrt(5))) / log10((1+sqrt(5))/2)))
