jwodder/euler

commit 0b96ddb72a8766354f0815b25ab3367ad282a7ad

Added the problem titles to the docstrings

euler0013.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
-"""Work out the first ten digits of the sum of the following one-hundred
+"""Large sum
+
+   Work out the first ten digits of the sum of the following one-hundred
    50-digit numbers.
 
        37107287533902102798797998220837590246510135740250

dev/euler0044.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
-r"""Pentagonal numbers are generated by the formula, $P_n=n(3n-1)/2$.  The
+r"""Pentagon numbers
+
+    Pentagonal numbers are generated by the formula, $P_n=n(3n-1)/2$.  The
     first ten pentagonal numbers are:
 
     $$1, 5, 12, 22, 35, 51, 70, 92, 117, 145, \ldots$$

dev/euler0204.py
@@ -1,3 +1,21 @@
+#!/usr/bin/python
+"""Generalised Hamming Numbers
+
+   A Hamming number is a positive number which has no prime factor larger than
+   5.
+   
+   So the first few Hamming numbers are 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15.
+
+   There are 1105 Hamming numbers not exceeding $10^8$.
+
+   We will call a positive number a generalised Hamming number of type $n$, if
+   it has no prime factor larger than $n$.
+   
+   Hence the Hamming numbers are the generalised Hamming numbers of type 5.
+
+   How many generalised Hamming numbers of type 100 are there which don't
+   exceed $10^9$?"""
+
 import sys
 sys.path.insert(1, sys.path[0] + '/..')
 from eulerlib import nPrimes, cross, product

dev/euler0214.py
@@ -1,3 +1,30 @@
+#!/usr/bin/python
+r"""Totient Chains
+
+    Let $φ$ be Euler's totient function, i.e. for a natural number $n$, $φ(n)$
+    is the number of $k$, $1\leq k\leq n$, for which $\gcd(k,n) = 1$.
+
+    By iterating $φ$, each positive integer generates a decreasing chain of
+    numbers ending in 1.
+
+    E.g. if we start with 5 the sequence 5,4,2,1 is generated.
+
+    Here is a listing of all chains with length 4:
+
+        5,4,2,1
+        7,6,2,1
+        8,4,2,1
+        9,6,2,1
+        10,4,2,1
+        12,4,2,1
+        14,6,2,1
+        18,6,2,1
+
+    Only two of these chains start with a prime, their sum is 12.
+
+    What is the sum of all primes less than 40000000 which generate a chain of
+    length 25?"""
+
 import itertools
 import sys
 sys.path.insert(1, sys.path[0] + '/..')

euler0001.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
-"""If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+"""Multiples of 3 and 5
+
+   If we list all the natural numbers below 10 that are multiples of 3 or 5, we
    get 3, 5, 6 and 9.  The sum of these multiples is 23.
 
    Find the sum of all the multiples of 3 or 5 below 1000."""

euler0002.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
-"""Each new term in the Fibonacci sequence is generated by adding the previous
+"""Even Fibonacci numbers
+
+   Each new term in the Fibonacci sequence is generated by adding the previous
    two terms. By starting with 1 and 2, the first 10 terms will be:
 
    $$1, 2, 3, 5, 8, 13, 21, 34, 55, 89, \ldots$$

euler0003.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
-"""The prime factors of 13195 are 5, 7, 13 and 29.
+"""Largest prime factor
+
+   The prime factors of 13195 are 5, 7, 13 and 29.
 
    What is the largest prime factor of the number 600851475143?"""
 

euler0004.cpp
@@ -1,4 +1,6 @@
-/* A palindromic number reads the same both ways. The largest palindrome made
+/* Largest palindrome product
+ *
+ * A palindromic number reads the same both ways. The largest palindrome made
  * from the product of two 2-digit numbers is $9009 = 91\times 99$.
  *
  * Find the largest palindrome made from the product of two 3-digit numbers.

commit a1a5b0cf846e3ddfb5d29b7ad357fc27d5d98847

Fixed a bug in euler0013.py caused by commit 0b96ddb72a8766354f0815b25ab3367ad282a7ad

euler0013.py
@@ -105,6 +105,6 @@
        20849603980134001723930671666823555245252804609722
        53503534226472524250874054075591789781264330331690"""
 
-#nums = map(int, __doc__.splitlines()[3:])  # This is efficient enough, but...
-nums = map(lambda s: int(s.strip()[:12]), __doc__.splitlines()[3:])
+#nums = map(int, __doc__.splitlines()[5:])  # This is efficient enough, but...
+nums = map(lambda s: int(s.strip()[:12]), __doc__.splitlines()[5:])
 print str(sum(nums))[:10]