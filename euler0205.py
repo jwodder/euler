5ac7063526971828b594293d6de3f0ddd4ce649b
John T. Wodder II <jwodder@sdf.lonestar.org>
2014-09-25 18:05:35 -0400

    Solved #205

---
+++ euler0205.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+"""Dice Game
+
+   Peter has nine four-sided (pyramidal) dice, each with faces numbered 1, 2,
+   3, 4.
+
+   Colin has six six-sided (cubic) dice, each with faces numbered 1, 2, 3, 4,
+   5, 6.
+
+   Peter and Colin roll their dice and compare totals: the highest total wins.
+   The result is a draw if the totals are equal.
+
+   What is the probability that Pyramidal Pete beats Cubic Colin?  Give your
+   answer rounded to seven decimal places in the form 0.abcdefg"""
+
+from collections import defaultdict
+from eulerlib    import cross
+
+# 9d4 > 6d6
+
+peteRolls = defaultdict(int)
+for roll in cross(*[range(1,5)]*9):
+    peteRolls[sum(roll)] += 1
+
+colinRolls = defaultdict(int)
+for roll in cross(*[range(1,7)]*6):
+    colinRolls[sum(roll)] += 1
+
+prob = 0
+denom = 6**6 * 4**9
+for c in xrange(6, 37):
+    colinProb = colinRolls[c]
+    peteProb = sum(peteRolls[p] for p in xrange(c+1, 37))
+    prob += colinProb * peteProb
+
+probDec = (prob * 10**8) // denom
+print '0.%07d' % (probDec // 10 + (probDec % 10 >= 5),)
