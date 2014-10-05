8cc1c1163b73b9f244ac0decae12c29b94ad8c84
John T. Wodder II <jwodder@sdf.lonestar.org>
2014-10-04 20:11:25 -0400

    Solved #56

---
+++ euler0056.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+"""Powerful digit sum
+
+   A googol ($10^{100}$) is a massive number: one followed by one-hundred
+   zeros; $100^{100}$ is almost unimaginably large: one followed by two-hundred
+   zeros.  Despite their size, the sum of the digits in each number is only 1.
+
+   Considering natural numbers of the form, $a^b$, where $a,b < 100$, what is
+   the maximum digital sum?"""
+
+print max(sum(int(c) for c in str(a**b)) for a in xrange(2,100)
+					 for b in xrange(1,100))
