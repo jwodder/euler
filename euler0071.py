6b6d62b48674d487fefd0eb78806816863c075c1
John T. Wodder II <jwodder@sdf.lonestar.org>
2014-10-15 15:57:00 -0400

    Solved #71

---
+++ euler0071.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+r"""Ordered fractions
+
+    Consider the fraction, $n/d$, where $n$ and $d$ are positive integers.  If
+    $n<d$ and $HCF(n,d)=1$, it is called a reduced proper fraction.
+
+    If we list the set of reduced proper fractions for $d\leq 8$ in ascending
+    order of size, we get:
+
+    $$1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, \mathbf{2/5}, 3/7, 1/2, 4/7, 3/5,
+    5/8, 2/3, 5/7, 3/4, 4/5, 5/6, 6/7, 7/8$$
+
+    It can be seen that $2/5$ is the fraction immediately to the left of $3/7$.
+
+    By listing the set of reduced proper fractions for $d\leq 1,000,000$ in
+    ascending order of size, find the numerator of the fraction immediately to
+    the left of $3/7$."""
+
+from eulerlib import gcd
+
+(maxN, maxD) = (0,1)
+for d in xrange(2, 1000001):
+    n = (3*d - 1) // 7
+    if gcd(n,d) == 1 and n * maxD > maxN * d:
+	(maxN, maxD) = (n,d)
+print maxN
