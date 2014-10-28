339c3eac2a15348c8a8a77c14b15e2e57594dc74
John T. Wodder II <jwodder@sdf.lonestar.org>
2014-10-27 21:37:29 -0400

    Solved #173

---
+++ euler0173.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+"""Using up to one million tiles how many different "hollow" square laminae can
+   be formed?
+
+   We shall define a square lamina to be a square outline with a square "hole"
+   so that the shape possesses vertical and horizontal symmetry.  For example,
+   using exactly thirty-two square tiles we can form two different square
+   laminae:
+
+			     ######  #########
+			     ######  #       #
+			     ##  ##  #       #
+			     ##  ##  #       #
+			     ######  #       #
+			     ######  #       #
+				     #       #
+				     #       #
+				     #########
+
+   With one-hundred tiles, and not necessarily using all of the tiles at one
+   time, it is possible to form forty-one different square laminae.
+
+   Using up to one million tiles how many different square laminae can be
+   formed?"""
+
+maxTiles = 1000000
+qty = 0
+for start in xrange(3, maxTiles//4 + 2):
+    tilesLeft = maxTiles - (start-1)*4
+    qty += 1
+    i = start+2
+    while tilesLeft >= (i-1)*4:
+	tilesLeft -= (i-1)*4
+	qty += 1
+	i += 2
+print qty
