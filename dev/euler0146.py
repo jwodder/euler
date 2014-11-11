c7b351c6f39f827c35fbe5628194e3eac07e489c
John T. Wodder II <jwodder@sdf.lonestar.org>
2014-11-11 13:29:37 -0500

    Solved #134

--- dev/euler0146.py
+++ dev/euler0146.py
@@ -19,6 +19,7 @@ consecutive = (piter.next(), piter.next(), piter.next(),
 
 accum = 0
 for n in xrange(10, supN, 2):
+    if n % 3 == 0 or n % 7 in (0,3,4) or n % 13 == 0: continue
     sq = n*n
     while sq+1 > consecutive[0]:
 	consecutive = consecutive[1:] + (piter.next(),)
---
+++ primes/euler0134.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+r"""Prime pair connection
+
+    Consider the consecutive primes $p_1 = 19$ and $p_2 = 23$.  It can be
+    verified that 1219 is the smallest number such that the last digits are
+    formed by $p_1$ whilst also being divisible by $p_2$.
+
+    In fact, with the exception of $p_1 = 3$ and $p_2 = 5$, for every pair of
+    consecutive primes, $p_2 > p_1$, there exist values of $n$ for which the
+    last digits are formed by $p_1$ and $n$ is divisible by $p_2$.  Let $S$ be
+    the smallest of these values of $n$.
+
+    Find $\sum S$ for every pair of consecutive primes with $5\leq p_1\leq
+    1000000$."""
+
+import sys; sys.path.insert(1, sys.path[0] + '/..')
+from eulerlib import primeIter, modInverse
+
+piter = primeIter()
+piter.next()
+piter.next()
+p1 = piter.next()
+p2 = piter.next()
+nextMag = 10
+
+accum = 0
+while p1 <= 1000000:
+    while nextMag < p1:
+	nextMag *= 10
+    accum += p1 + nextMag * ((p2 - p1) * modInverse(nextMag, p2) % p2)
+    (p1, p2) = (p2, piter.next())
+print accum
