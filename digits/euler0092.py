502900c312e6185c87e87292d210a458af2f468c
John T. Wodder II <jwodder@sdf.lonestar.org>
2014-11-25 22:26:25 -0500

    Got euler0092.py to run in less than a minute

--- digits/euler0092.py
+++ digits/euler0092.py
@@ -16,14 +16,32 @@
 
    How many starting numbers below ten million will arrive at 89?"""
 
-ones = set([44,32,13,10,1])
-eightyNines = set([85,89,145,42,20,4,16,37,58])
+from collections import defaultdict
+
+def digits(n):
+    ds = []
+    while n > 0:
+	ds.append(n % 10)
+	n //= 10
+    ds.sort()
+    return tuple(ds)
+
+ones = set(map(digits, [44,32,13,10,1]))
+
+eightyNines = defaultdict(set)
+for n in [85,89,145,42,20,4,16,37,58]:
+    eightyNines[digits(n)].add(n)
 
 for i in xrange(2,10000000):
-    chain = []
-    j = i
+    j = digits(i)
+    chain = [(j,i)]
     while j not in ones and j not in eightyNines:
-	chain.append(j)
-	j = sum(int(c)*int(c) for c in str(j))
-    (ones if j in ones else eightyNines).update(chain)
-print len(eightyNines)
+	i = sum(d*d for d in j)
+	j = digits(i)
+	chain.append((j,i))
+    if j in ones:
+	ones.update(x for x,_ in chain)
+    else:
+	for x,y in chain:
+	    eightyNines[x].add(y)
+print sum(len(s) for s in eightyNines.itervalues())
