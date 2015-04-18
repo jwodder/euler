77c44cc060458158493942ec8922f788f2580d33
John T. Wodder II <jwodder@sdf.lonestar.org>
2015-04-18 00:27:49 -0400

    Added problem description to euler0117.py

--- euler0117.py
+++ euler0117.py
@@ -1,4 +1,23 @@
 #!/usr/bin/python
+"""Red, green, and blue tiles
+
+   Using a combination of black square tiles and oblong tiles chosen from: red
+   tiles measuring two units, green tiles measuring three units, and blue tiles
+   measuring four units, it is possible to tile a row measuring five units in
+   length in exactly fifteen different ways.
+
+       # # # # #    RR # # #    # RR # #    # # RR #
+
+       # # # RR     RR RR #     RR # RR     # RR RR
+
+       GGG # #      # GGG #     # # GGG     RR GGG
+
+       GGG RR       BBBB #      # BBBB
+
+   How many ways can a row measuring fifty units in length be tiled?
+
+   NOTE: This is related to Problem 116."""
+
 cache = [None] * 51
 
 def repl(i):
