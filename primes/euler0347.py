98100b454090df0b0bfaa4d7d75de4bf7b82b08e
John T. Wodder II <jwodder@sdf.lonestar.org>
2015-04-19 00:00:04 -0400

    Added problem description to euler0347.py

--- primes/euler0347.py
+++ primes/euler0347.py
@@ -1,7 +1,26 @@
+#!/usr/bin/python
+r"""Largest integer divisible by two primes
+
+    The largest integer $\leq 100$ that is only divisible by both the primes 2
+    and 3 is 96, as $96=32*3=2^5*3$.  For two *distinct* primes $p$ and $q$ let
+    $M(p,q,N)$ be the largest positive integer $\leq N$ only divisible by both
+    $p$ and $q$ and $M(p,q,N)=0$ if such a positive integer does not exist.
+
+    E.g. $M(2,3,100)=96$.
+
+    $M(3,5,100)=75$ and not 90 because 90 is divisible by 2, 3 and 5.
+
+    Also $M(2,73,100)=0$ because there does not exist a positive integer $\leq
+    100$ that is divisible by both 2 and 73.
+
+    Let $S(N)$ be the sum of all distinct $M(p,q,N)$.  $S(100)=2262$.
+
+    Find $S(10 000 000)$."""
+
+import sys; sys.path.insert(1, sys.path[0] + '/..')
 from eulerlib import primeIter, cross
 
 n = 10000000
-#n = 100
 
 def powers(p,n):
     pk = p
