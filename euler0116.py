fd22635286acf4e1acbda86841c5c3506e1eb5ce
John T. Wodder II <jwodder@sdf.lonestar.org>
2015-05-24 20:57:01 -0400

    Made euler0116.py slightly faster/more efficient

--- euler0116.py
+++ euler0116.py
@@ -29,11 +29,9 @@
    NOTE: This is related to Problem 117."""
 
 def replacements(n, size):
-    cache = [None] * (n+1)
-    def repl(i):
-        if cache[i] is None:
-            cache[i] = 1 + sum(repl(j) for j in xrange(i-size+1))
-        return cache[i]
-    return repl(n) - 1
+    repl = []
+    for i in xrange(n+1):
+        repl.append(1 + sum(repl[:max(i-size+1,0)]))
+    return repl[-1] - 1
 
 print replacements(50,2) + replacements(50,3) + replacements(50,4)
