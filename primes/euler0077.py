bc4366ad55ee3ab497791283317b4b25e976a25b
John T. Wodder II <jwodder@sdf.lonestar.org>
2015-09-27 14:59:18 -0400

    Solved #77

---
+++ primes/euler0077.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+"""Prime summations
+
+   It is possible to write ten as the sum of primes in exactly five different
+   ways:
+
+       7 + 3
+       5 + 5
+       5 + 3 + 2
+       3 + 3 + 2 + 2
+       2 + 2 + 2 + 2 + 2
+
+   What is the first value which can be written as the sum of primes in over
+   five thousand different ways?"""
+
+import bisect
+import sys; sys.path.insert(1, sys.path[0] + '/..')
+from   eulerlib import primeIter
+
+cache = {}
+
+def primesums(n):
+    def subp(n,p):
+        """Returns the number of ways `n` can be written as a sum of primes
+           each of which is no greater than `p`"""
+        if n == 0:
+            return 1
+        elif n < 2:
+            return 0
+        else:
+            i = bisect.bisect(primes, min(n,p))
+            if (n,i) not in cache:
+                cache[(n,i)] = sum(subp(n-primes[j], primes[j])
+                                   for j in xrange(i-1, -1, -1))
+            return cache[(n,i)]
+    return subp(n,n)
+
+bound = 1000
+primes = list(primeIter(bound=bound))
+n = 2
+while primesums(n) <= 5000:
+    n += 1
+    if n >= bound:
+        bound *= 10
+        primes = list(primeIter(bound=bound))
+print n
