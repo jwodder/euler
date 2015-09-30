89f6e13c4314b51135b0514dfc0750cd0b48ed59
John T. Wodder II <jwodder@sdf.lonestar.org>
2015-09-29 23:27:59 -0400

    Got euler0003.py off oldprimes.py (and made it slightly faster!)

--- primes/euler0003.py
+++ primes/euler0003.py
@@ -7,11 +7,11 @@
 
 import sys
 sys.path.insert(1, sys.path[0] + '/..')
-from   eulerlib.oldprimes import primeIter
+from   eulerlib import allprimes
 
 n = 600851475143
 
-for p in primeIter():
+for p in allprimes():
     while n % p == 0:
         n //= p
         if n == 1:
